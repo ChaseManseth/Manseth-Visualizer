2672127a4b2400507be3ebad70b59c604ebfe149
ChaseManseth <chasemanseth@gmail.com>
2017-04-21 14:27:39 -0600

    Fixed problem
    Fixed the annoying black line in the middle of the analyser. Added a
    volume bar that changes the multiplier of the amplitude. Increase the
    degree to 10 but added a warning for anything over 5

--- js/audio.js
+++ js/audio.js
@@ -1,13 +1,11 @@
-console.clear();
-
 // Create audio context
 var AUDIO = new (window.AudioContext || window.webkitAudioContext)();
-if(!AUDIO) console.error('Web Audio API not supported :(');
+if (!AUDIO) {console.error('Web Audio API not supported :(');}
 
-// Create and configure analyzer node and storage buffer
-var analyzer = AUDIO.createAnalyser();
-analyzer.fftSize = 512;
-var bufferLength = analyzer.frequencyBinCount;
+// Create and configure analyser node and storage buffer
+var analyser = AUDIO.createAnalyser();
+analyser.fftSize = 512;
+var bufferLength = analyser.frequencyBinCount;
 console.log(bufferLength);
 var dataArray = new Uint8Array(bufferLength);
 
@@ -21,7 +19,7 @@ function createBars(numBars) {
     
     console.log('numBars', numBars);
     
-    for(var i = 0; i < numBars; i++) {
+    for (var i = 0; i <= numBars; i++) {
         var a = document.createElement('div');
         a.classList.add('bar');
         
@@ -32,81 +30,108 @@ function createBars(numBars) {
     return barCollection;
 }
 
-// $segs is an array of all the bars creates
-var $segs = 0;
+// $bars is an array of all the bars creates
+var $bars = 0;
 // snip is trimming the last blank buffer length
 var snip = 99;
 // n is the degree of the smoothing algorithm
 var n = 3;
 
-function degreeSilder(x) {
-     document.getElementById("viz").innerHTML='';
-    $segs = 0;
-    $segs = createBars((((bufferLength - snip) * 2) - 1) * x); 
-    style();
-    n = x;
+// Ask a question once for higher degrees
+var ans = "false";
+function degreeSlider(x) {
+    if(x > 5 && ans === "false") {
+        toggleState();
+        ans = prompt("Warning, increasing the degree over 5 may lag out your browser. Are you sure you want to procede? Type true or false for your response");
+        toggleState();
+    }
+    
+    if(ans) {
+        // Removes all elements
+        document.getElementById("viz").innerHTML='';
+        // Sets the seg array to 0 and creates them again with the proper amount of segs
+        $bars = 0;
+        $bars = createBars((((bufferLength - snip) -1) * 2) * x);
+        // Restyle it because we have more or less segs now and must adjust
+        style();
+        n = x;
+    } else {
+        // Removes all elements
+        document.getElementById("viz").innerHTML='';
+        // Sets the seg array to 0 and creates them again with the proper amount of segs
+        $bars = 0;
+        $bars = createBars((((bufferLength - snip) -1) * 2) * x);
+        // Restyle it because we have more or less segs now and must adjust
+        style();
+        n = x;
+    }
 }
 
 
 // Main render and update method
-function update(b) {
-    analyzer.getByteFrequencyData(dataArray);
+function update() {
+    analyser.getByteFrequencyData(dataArray);
     
     // Copy the dataArray without high frequencies
     var low = [];
-    var len = bufferLength - snip;
+    var len = (bufferLength - snip);
     
     for(var i = 0; i <= len; i++) {
         low.push(dataArray[i]);
     }
     
-    // Use Scott's smoothing function
+    // Use Scott's parabolic approximation function
     var smooth = [];
-    smooth.push(low[0]);
-    for(var i = 1; i < low.length -1; i += 2) {
-        var mult = 0.5;
+    smooth.push(Math.floor(low[0]));
+    
+    for(var i = 1; i < low.length - 1; i += 2) {
+
         var c = low[i];
         var a = (low[i+1] + low[i-1] - 2*low[i]) / 2.0;
         var b = (low[i+1] - low[i-1]) / 2.0;
+        
         for(var j = 1; j < n; j++) {
-            smooth.push(a * (1.0/n) * (1.0/n) * j + b * (1.0/n) * (-1) * j + c);
+            smooth.push(Math.floor(a * (1.0/n) * (1.0/n) * j + b * (1.0/n) * (-1) * j + c));
         }
-//        smooth.push(a * mult * mult + b * mult * (-1) + c);
-        smooth.push(low[i]);
+        
+        smooth.push(Math.floor(low[i]));
         for(var j = 1; j < n; j++) {
-            smooth.push(a * (1.0/n) * (1.0/n) * j + b * (1.0/n) * j + c);
+            smooth.push(Math.floor(a * (1.0/n) * (1.0/n) * j + b * (1.0/n) * j + c));
         }
-//        smooth.push(a * mult * mult + b * mult + c);
-        smooth.push(low[i+1]);
+        
+        smooth.push(Math.floor(low[i+1]));
     }
     
-//    console.log("smooth length : " + smooth.length);
-    
+    // Translate modified array
     var a = 0;
+    if(a >= $bars.length) {
+        a = 0;
+    }
     // First half going clockwise
     for(var i = 0; i < smooth.length; i++) {
-        var width = smooth[i];        
-        $segs[a].css('height', width);
+        var width = smooth[i]; 
+//        console.log("i: " + i + " || "+ width);
+        $bars[a].css('height', width);
         a++;
     }
     
     // Second half going counter-clockwise
     for(var i = smooth.length - 1; i > 0; i--) {
         var width = smooth[i];        
-        $segs[a].css('height', width);
+        $bars[a].css('height', width);
         a++;
     }
 }
 
 // Initiate function
 function init() {
-    // Connect audio to analyzer and analyze audio
+    // Connect audio to analyser and analyze audio
     console.log('init');
     var source = AUDIO.createMediaElementSource(aud);
-    source.connect(analyzer);
-    analyzer.connect(AUDIO.destination);
+    source.connect(analyser);
+    analyser.connect(AUDIO.destination);
     
-    $segs = createBars((((bufferLength - snip) * 2) - 1) * n);    
+    $bars = createBars((((bufferLength - snip) -1) * 2) * n);  
     style();
     start(true);
 }
@@ -126,18 +151,18 @@ function start(x) {
 
 // Rotate and properly position the bars into a circle with a set radius
 function style() {
-    var step = Math.PI * 2 / $segs.length;
+    var step = Math.PI * 2 / $bars.length;
     var radius = 160;
     var angle = 0;
     
-    for(var i = 0; i < $segs.length; i++) {
-        var $elem = $segs[i];
-        
+    for(var i = 0; i < $bars.length; i++) {
+        var $elem = $bars[i];
+        // Maths
         var x = Math.round(radius * Math.cos(angle));
         var y = Math.round(radius * Math.sin(angle));
         $elem.css('left', x + 'px');
         $elem.css('top', y + 'px');
-        var rot = -90 + ((360 / $segs.length) * i);
+        var rot = -90 + ((360 / $bars.length) * i);
         $elem.css('transform', 'rotate(' + rot + 'deg)'); 
         angle += step;
 
@@ -162,16 +187,36 @@ function toggleState() {
 $(document).ready(function() {
     init();
     
+    // Degree Slider
+        // Get the silder and the input to store values
+        var degSlider = $('#degree');
+        var degVal = $('#degreeVal');
+        degVal.val(degSlider.val());
+        // When ever the silder is used change value in the input and run function to reload
+        degSlider.on("change", function() {
+            degVal.val(degSlider.val());
+            degreeSlider(degSlider.val());
+        });
     
-    var degSilder = $('#degree');
-    var degVal = $('#degreeVal');
-    degVal.val(degSilder.val());
-    degSilder.on("click", function() {
-        degVal.val(degSilder.val());
-        degreeSilder(degSilder.val());
-    });
+    // Volume Slider
+        var volSlider = $('#volume');
+        var volVal = $('#volumeVal');
+        volVal.val(volSlider.val());
+        // When ever the silder is used change value in the input and run function to reload
+        volSlider.on("input", function() {
+            volVal.val(volSlider.val());
+            var vol = volSlider.val() / 100;
+            aud.volume = vol;
+        });
     
 //    setInterval(function() {console.log(Math.floor(aud.currentTime))}, 1000);
+});
+
+// Event listener for space keypress to play and pause
+$(document).keypress(function(e) {
+  if(e.which == 32) {
+    toggleState();
+  }
 });
 
 // Toggle played and paused states
